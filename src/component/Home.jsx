1c61aad8b7e7d8a5ef3c0cb5dee0de41e75e6a7e
iamogbumitchel <mitchelogbu47@gmail.com>
2024-01-17 18:04:38 +0100

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -176,7 +176,7 @@ const Home = () => {
         </div>
         <div>
           {/* <form ref={form} onSubmit={submitHandler}> */}
-          <form method="POST" name="contact" data-netlify="true">
+          <form method="POST" name="Any form" data-netlify="true">
             {/* <label>Name</label> */}
             <input
               type="text"
